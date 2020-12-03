74d37eea5c4d53bb72a7b51fe1b63b7e33098fc4
Francisco Vallejo Luna <francisco.vallejo-luna@microfocus.com>
2020-12-03 14:30:57 +0100

    day3 update

---
+++ 2020/Day3/Part I/day3.py
@@ -0,0 +1,53 @@
+INPUT_FILE = "2020/Day3/inputdata"
+VERTICAL_STEP = 1
+HORIZONTAL_STEP = 3
+VERTICAL_START = 0
+HORIZONTAL_START = 0
+
+def load_data(filename):
+    try:
+        with open(filename, 'r') as f:
+            return [line.strip() for line in f]
+    except:
+        print("exception on file read")
+        return []
+
+
+def loop_method(slope):
+    slope_high = len(slope)
+    slope_matrix_width = len(slope[0])
+    #we can ignore width, since it repeats to both sides (left and right)
+    hori = HORIZONTAL_START
+    trees = 0
+    blanks = 0
+
+    print(f"Slope scan starting...")
+
+    for high in range(VERTICAL_START, slope_high, VERTICAL_STEP):
+        print(f"Scanning slope on position {high} {hori}")
+        thing = slope[high][hori]
+        print(f"Found {thing} ( dot means blank, hash means tree )")
+        if (thing == "."): #it's blank
+            blanks = blanks+1
+        elif (thing == "#"): #it's a tree
+            trees = trees+1
+        else: #just in case, for part II
+            pass
+        #calculate hori for the next iteration
+        hori = (hori + HORIZONTAL_STEP) % slope_matrix_width
+        print(f"Horizontal coordenate for next iteration will be {hori}")
+
+    print(f"Slope scan completed!")
+    
+    return trees, blanks
+
+
+def main():
+    slope = load_data(INPUT_FILE)
+    trees, blanks = loop_method(slope)
+    print(f"Tree count equals to {trees} and {blanks} blanks")
+
+if __name__ == "__main__":
+    main()
+
+print("NeoTheRack - completed!")
